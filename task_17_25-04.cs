88a8937690af68cfb3b305b5d11d06a6c183ef4c
AlexUstosu <44317065+AlexUstosu@users.noreply.github.com>
2023-04-25 21:33:22 +0300

    Задача с координатами точки и с массивом

---
+++ task_17_25-04.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleApp2;
+using System;
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        struct Point
+        {
+           public double x;
+           public double y;
+        }
+
+        static void Main(string[] args)
+        {
+            Point B = new Point();
+
+            Console.WriteLine("Введите координаты точки");
+
+            Console.WriteLine("x - ");
+            B.x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("y - ");
+            B.y = Convert.ToDouble(Console.ReadLine());
+
+            double f = 0;
+            if (Math.Abs(B.x) > 1)
+            {
+                f = Math.Pow(Math.Sin(Math.Pow(B.x,3)), 2 );
+            }
+            else if(Math.Abs(B.x) <= 1)
+            {
+                f = Math.Sqrt(6 * Math.Asin(Math.Pow(B.x,7)) + 
+                    4.5F * Math.Pow(B.x, 6) +
+                    4D * Math.Pow(B.x, 2) + 2 );
+            }
+            else
+            {
+                Console.WriteLine("Ошибка!");
+            }
+
+            if(Math.Abs(f - B.y) < Math.Pow(10, -3))
+                Console.WriteLine($"f(x) - {f}, где погрешность < eps");
+            else
+                Console.WriteLine($"f(x) - {f}, где погрешность > eps");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task_29_25-04.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp2;
+using System;
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array = new int[20]; 
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 11);
+            }
+
+            Console.WriteLine("Входной массив - > ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                Console.Write(" ");
+            }
+
+            //Нахождение среднего арифм.
+            double sr = 0;
+            for (int i = 0; i < array.Length; i++)
+                sr += array[i];
+
+            sr /= array.Length;
+
+            //Нахождение кол-ва
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (sr < array[i])
+                    count++;
+            }
+
+            //Нахождение процентного отношения
+            double per;
+
+            per = ((double)count / array.Length)*100;
+
+            Console.WriteLine($"\nСреднее арифм. - > {sr}");
+            Console.WriteLine($"Процентное отношение - > {per}%");
+
+            Console.ReadKey();
+        }
+    }
+}
